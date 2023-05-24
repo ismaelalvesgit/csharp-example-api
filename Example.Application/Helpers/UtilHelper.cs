c6e0ecf1913918e82fd3b31dca8433c0ed7891bc
ismael alves <cearaismael1997@gmail.com>
2023-05-23 23:24:22 -0300

    add helper Serialize

---
+++ Example.Application/Helpers/UtilHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Example.Application.Helpers
+{
+    public static class UtilHelper
+    {
+
+        public static string Serialize<TValue>(TValue value) {
+            var jsonOptions = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            return JsonSerializer.Serialize(value, jsonOptions);
+        }
+    }
+}
