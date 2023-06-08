013cf3dee4c2f51711c51a09959070a4330079b5
ismael alves <cearaismael1997@gmail.com>
2023-06-08 18:15:31 -0300

    add model ConsumerMessageError

---
+++ Example.BackgroundTasks/Model/ConsumerMessageError.cs
@@ -0,0 +1,8 @@
+﻿namespace Example.BackgroundTasks.Model
+{
+    public class ConsumerMessageError
+    {
+        public string? Message { get; set; }
+        public List<Exception>? Exceptions { get; set; }
+    }
+}
